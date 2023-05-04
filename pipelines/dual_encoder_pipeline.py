74b6a7cab814b3afe866ec829e5127d41297c9ba
Johanna Karras <31581146+johannakarras@users.noreply.github.com>
2023-05-04 14:17:35 -0700

    Add files via upload

--- pipelines/dual_encoder_pipeline.py
+++ pipelines/dual_encoder_pipeline.py
@@ -461,10 +461,6 @@ class StableDiffusionImg2ImgPipeline(DiffusionPipeline):
             list of `bool`s denoting whether the corresponding generated image likely represents "not-safe-for-work"
             (nsfw) content, according to the `safety_checker`.
         """
-        message = "Please use `image` instead of `init_image`."
-        init_image = deprecate("init_image", "0.12.0", message, take_from=kwargs)
-
-        image = init_image or image
 
         # 1. Check inputs
         self.check_inputs(prompt, strength, callback_steps)
